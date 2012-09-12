9661a085225a4f469c3e9aaacba6c5ead1336943
Bronson Oka <abunai59@gmail.com>
2012-09-12 13:14:07 -1000

    Implement main
    Begin implementation
    
    This should handle the initial intake of arguments
    as well as taking in user input during run time

--- src/main/java/ioProcessor/MainProcessor.java
+++ src/main/java/ioProcessor/MainProcessor.java
@@ -1,13 +1,29 @@
-package ioProcessor;
 
+package ioProcessor;
+/**
+ * The main process for the EE260 debugger
+ * @author boka
+ */
 public class MainProcessor {
 
 	/**
-	 * @param args
+	 * @param args to run the program
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Process arguments
+		if (!args[0].isEmpty()) {
+			if (args[0].endsWith(".s")) {
+				System.out.println("Running with input file: " + args[0]);
+				
+				// TODO iomain:  Pass Ejay's code the file here through constructor
+			} 
+			else if (args[0].equals("-h")) {
+				System.out.println("Help menu:");
+				System.out.println("");
+			}
+		} 
+		else {
+			System.out.println("Use command -h for help.");
+		}
 	}
-
 }
