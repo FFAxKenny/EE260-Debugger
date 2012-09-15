f7003027baf2fc7a25dd441e15a23972ce0c8ed8
Bronson Oka <abunai59@gmail.com>
2012-09-15 13:40:05 -1000

    Implement main:  whitespace fix
    Fix the whitespace errors with java formatter

--- src/main/java/ioProcessor/MainProcessor.java
+++ src/main/java/ioProcessor/MainProcessor.java
@@ -1,7 +1,8 @@
-
 package ioProcessor;
+
 /**
  * The main process for the EE260 debugger
+ * 
  * @author boka
  */
 public class MainProcessor {
@@ -14,14 +15,15 @@ public class MainProcessor {
 		if (!args[0].isEmpty()) {
 			if (args[0].endsWith(".s")) {
 				System.out.println("Running with input file: " + args[0]);
-				
-				// TODO iomain:  Pass Ejay's code the file here through constructor
-			} 
+
+				// TODO iomain: Pass Ejay's code the file here through
+				// constructor
+			}
 			else if (args[0].equals("-h")) {
 				System.out.println("Help menu:");
 				System.out.println("");
 			}
-		} 
+		}
 		else {
 			System.out.println("Use command -h for help.");
 		}
