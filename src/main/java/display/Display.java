d9f656247d203607410bfeedfd403236d680d7d6
Bronson Oka <abunai59@gmail.com>
2012-09-16 13:06:33 -1000

    Display no longer uses jcurses
    Tired of looking around for help with
    testing jcurses.  There's just no way to do it.
    
    Using a line by line implementation now.

--- src/main/java/display/Display.java
+++ src/main/java/display/Display.java
@@ -1,10 +1,7 @@
 package display;
 
-import jcurses.system.CharColor;
-import jcurses.widgets.DefaultLayoutManager;
-import jcurses.widgets.Label;
-import jcurses.widgets.WidgetsConstants;
-import jcurses.widgets.Window;
+import java.awt.Label;
+import java.awt.Window;
 
 /**
  * Displays data for EE260 debugger
@@ -12,58 +9,20 @@ import jcurses.widgets.Window;
  * @author boka
  */
 public class Display {
-	// Static values
-	private static final char PRGM_CTR = 'p';
-	private static final char EXEC_CODE = 'c';
-	private static final char GPR_A = 'a';
-	private static final char GPR_B = 'b';
-	private static final char MEM_REG = 'r';
-	private static final char I_O = 'i';
-	private static final char MSG = 'm';
-	private static final int WINDOW_HEIGHT = 24;
-	private static final int WINDOW_WIDTH = 80;
-	private static final boolean BORDER = true;
-	private static final String WINDOW_TITLE = "Main Window";
-
-	// Member objects
-	private final Window mWindow;
-	private final DefaultLayoutManager mLayoutManager;
-
 	/**
 	 * Default constructor
 	 */
 	public Display() {
-		mWindow = new Window(WINDOW_WIDTH, WINDOW_HEIGHT, BORDER, WINDOW_TITLE);
-		mLayoutManager = new DefaultLayoutManager();
-		mLayoutManager.bindToContainer(mWindow.getRootPanel());
-		mLayoutManager.addWidget(new Label("hello", new CharColor(CharColor.WHITE, CharColor.BLACK)), 0, 0, 20, 3,
-				WidgetsConstants.ALIGNMENT_CENTER,
-				WidgetsConstants.ALIGNMENT_CENTER);
-		mWindow.show();
-		try {
-			Thread.currentThread().sleep(5000);
-		}
-		catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		mWindow.close();
-		clear();
 	}
-
+	
 	/**
-	 * Clears the display
+	 * Display a line of code 
 	 */
-	public void clear() {
+	public putCodeLine() {
+		
 	}
-
+	
 	/**
-	 * Updates the display
-	 * 
-	 * @param
+	 * Display a register
 	 */
-	public void update(char module, String value) {
-		switch (module) {
-		}
-	}
 }
--- src/test/java/display/TestDisplay.java
+++ src/test/java/display/TestDisplay.java
@@ -13,7 +13,6 @@ public class TestDisplay {
 
 	@Test
 	public void testClear() {
-		mDisplay.clear();
 	}
 
 }
