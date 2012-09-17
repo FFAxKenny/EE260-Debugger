12909918eec93efbe8c3d3465ad149918288d864
Ejay Tumacder <ejayhi@yahoo.com>
2012-09-17 02:52:22 -1000

    Complete some methods for CodeProcessor
    Also setup methods for Tiffany to finish.

--- src/main/java/CodeProcessor/CodeProcessor.java
+++ src/main/java/CodeProcessor/CodeProcessor.java
@@ -1,6 +1,7 @@
 package CodeProcessor;
 
 import java.util.HashMap;
+import java.util.List;
 
 import fileParser.RowData;
 import fileParser.SourceData;
@@ -17,11 +18,21 @@ import storage.Storage;
 public class CodeProcessor {
 
     private Storage mStorage;
+    int mPC;
     
+    /**
+     * Constructor for the CodeProcessor class.
+     * @param source A SourceData object gives the codeProcessor the data it needs
+     */
     public CodeProcessor(SourceData source) {
 	this.importSourceToStorage(source);
+	mPC = 0;
     }
     
+    /**
+     * Import source to storage within the code space.
+     * @param source
+     */
     public void importSourceToStorage(SourceData source) {
 	HashMap<String, RowData> sourceData = source.getSourceCode();
 	for(int i = 0; i < source.getSize(); i += 2) {
@@ -37,13 +48,54 @@ public class CodeProcessor {
 			Byte.parseByte(row.getMneumonic()));
 	    }
 	}
-	
+    }
+    
+    /**
+     * @return registers in a & b GPRs[0] = a, GPRs[1] = b
+     */
+    public char[] getGPRs() {
+	char[] GPRs = { mStorage.readRegister('a') , mStorage.readRegister('b') };
+	return GPRs;
+    }
+    
+    /**
+     * Writes to IO Space (0x40 to 0x7F)
+     * @param hexAddress
+     * @param hexValue
+     */
+    public void writeToIOSpace(String hexAddress, String hexValue) {
+	// TODO: Tiffany - please implement this
+    }
+    
+    /**
+     * Writes to Data Space (0x80 to 0xFF)
+     * @param hexAddress
+     * @param hexValue
+     */
+    public void writeToDataSpace(String hexAddress, String hexValue) {
+	// TODO: Tiffany - please implement this
+    }
+    
+    /**
+     * Will either expect 2 or 3 elements within the array.
+     * @return a String[] that desribes what is going on currently in the code.
+     * Note: it's assumed that the code has already been imported
+     */
+    public List<String[]> step() {
+	// TODO: Tiff - you need to implement this function by iterating through
+	// the code that's stored in the code space. Iterate using the PC. PC will
+	// also handle jumps to labels and such behavior.
     }
     
     // Executes the code according to the opCode parameter
     // If parameter data is an address then must be in hex format Eg. 0x4f 0xF3
     // If using TAB or TBA then parameterdata can be set to anything
     public void instruction(String opCode, String data) {
+	// TODO: Tiff - you have to finish what Brandon was doing here. This is where
+	// we simulate the behavior of the code. The behaviors are accessed through
+	// the opcode since that is how it is stored in storage.
+	
+    
 	String OP = opCode.toLowerCase();
 	String DATA = data.toLowerCase();
 	switch(OP) {
