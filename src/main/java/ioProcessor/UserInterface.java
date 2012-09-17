80d2bd4d60a18be872c8a31ccfd7fb903db09b62
Bronson Oka <abunai59@gmail.com>
2012-09-16 14:06:02 -1000

    Improve User Interface

--- src/main/java/ioProcessor/UserInterface.java
+++ src/main/java/ioProcessor/UserInterface.java
@@ -1,7 +1,7 @@
 package ioProcessor;
 
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * User input processor
@@ -9,7 +9,14 @@ import java.io.InputStreamReader;
  * @author boka
  */
 public class UserInterface {
-	private InputStreamReader mInputReader;
+	private static final String QUIT = "quit";
+	private static final String HELP = "help";
+	private static final String STEP = "step";
+	private static final String ERROR = "error";
+	private static final String CHANGE_IO = "io";
+	private static final String GET = "get";
+
+	private Scanner mScanner;
 
 	/**
 	 * Default constructor
@@ -24,11 +31,72 @@ public class UserInterface {
 	 * @param input
 	 */
 	private UserInterface(InputStream input) {
-		mInputReader = new InputStreamReader(input);
+		mScanner = new Scanner(input);
 	}
 
 	/**
-	 * Get a command from the user
+	 * Get a command from user input
 	 */
-	public void getCommand(){};
+	public String[] getCommand() {
+		return processCommand(getSplitLine());
+	}
+
+	/**
+	 * Process a command from split line
+	 */
+	public String[] processCommand(String[] splitLine) {
+		String[] command;
+
+		if (splitLine.length >= 1) {
+			switch (splitLine[0]) {
+				case "quit":
+					command = new String[] { QUIT };
+					break;
+				case "help":
+					command = new String[] { HELP };
+					break;
+				case "step":
+					if (splitLine.length == 1) {
+						command = new String[] { STEP };
+					}
+					else {
+						command = new String[] { STEP, splitLine[1] };
+					}
+					break;
+				case "io":
+					if (splitLine.length >= 3) {
+						command = new String[] { CHANGE_IO, splitLine[1],
+								splitLine[2] };
+					}
+					else {
+						command = new String[] { ERROR };
+					}
+					break;
+				case "get":
+					if (splitLine.length >= 3) {
+						command = new String[] { GET, splitLine[1],
+								splitLine[2] };
+					}
+					else {
+						command = new String[] { ERROR };
+					}
+					break;
+
+				default:
+					command = new String[] { ERROR };
+					break;
+			}
+		}
+		else {
+			command = new String[] { ERROR };
+		}
+		return command;
+	}
+
+	/**
+	 * Get a line from the user input and split
+	 */
+	private String[] getSplitLine() {
+		return mScanner.nextLine().trim().split("\\s");
+	}
 }
