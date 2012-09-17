34f5f256544114ab9d691a652bc19342940f7ea3
Bronson Oka <abunai59@gmail.com>
2012-09-17 11:01:30 -1000

    Complete MainProcessor implementation
    Now that CodeProcessor has been stubbed,
    MainProcessor implementation is complete
    
    Still need to handle the invalid key exception

--- src/main/java/ioProcessor/MainProcessor.java
+++ src/main/java/ioProcessor/MainProcessor.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 
+import CodeProcessor.CodeProcessor;
+
 import display.Display;
 
 import fileParser.ASMReader;
@@ -53,31 +55,65 @@ public class MainProcessor {
 			// Set up code space and initialize classes
 			ASMReader asmReader = new ASMReader(inputFile);
 			SourceData sourceData = asmReader.read();
-			// TODO send sourceData to Code Processor
-			// CodeProcessor codeProcessor = new CodeProcessor(sourceData);
-			
+			CodeProcessor codeProcessor = new CodeProcessor(sourceData);
+
 			// Set up user interface
 			UserInterface userInterface = new UserInterface();
 
 			// Set up Display
 			Display display = new Display();
-			
+
 			// Main loop
-			boolean done = false; // loop condition
-			while (!done) {
+			while (true) {
 				String[] command = userInterface.getCommand();
-				// TODO send command to codeProcessor
-				// String[] updates = codeProcessor.run(command);
-				// display.update(updates);
+
+				// If command is to quit
+				if (command[0] == UserInterface.QUIT) {
+					System.out.println("Terminating.");
+					return;
+				}
+
+				// If command is for help
+				else if (command[0] == UserInterface.HELP) {
+					System.out.println("Commands:");
+					System.out.println("quit (exits the program)");
+					System.out.println("help (shows this help menu)");
+					System.out.println("step <int>");
+					System.out.println("\t(steps the program int times)");
+					System.out.println("io <hex1> <hex2>");
+					System.out.println("\t(changes io at hex1 to value hex2)");
+					System.out.println("get \"gpr\" <char>");
+					System.out.println("\t(view value at gpr 'a' or 'b'");
+					System.out.println("get \"mem\" <hex>");
+					System.out.println("\t(view value at address <hex>");
+				}
+
+				// If there was an error
+				else if (command[0] == UserInterface.ERROR) {
+					System.out.println("Error processing command.");
+					System.out.println("For instructions enter command: help");
+				}
+				// If command is to step
+				else if (command[0] == UserInterface.STEP) {
+					int steps = Integer.parseInt(command[1]);
+					display.update(codeProcessor.step(steps));
+				}
+
+				else {
+					display.update(codeProcessor.runCommand(command));
+				}
+				// Separate lines
+				System.out.println();
 			}
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		catch (InvalidKeyException e) {
+		catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		}
-		catch (IllegalArgumentException e) {
+		catch (InvalidKeyException e) {
+			// TODO Fix the error in the project caused by this
 			e.printStackTrace();
 		}
 		finally {
