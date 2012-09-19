c538e220221680b84e1d66222860a3d185ad8b7d
Bronson Oka <abunai59@gmail.com>
2012-09-19 09:00:49 -1000

    fix whitespace in asmreader test

--- src/test/java/fileParser/ASMReaderTest.java
+++ src/test/java/fileParser/ASMReaderTest.java
@@ -10,27 +10,27 @@ import org.junit.Test;
 
 public class ASMReaderTest {
 
-    @Test
-    public void readTest() throws IOException, InvalidKeyException,
-	    IllegalArgumentException {
-	ASMReader reader = new ASMReader(new File("SampleData/AsmTiny.s"));
-	SourceData convertedAsmCode = reader.read();
-	RowData convertedRow0 = convertedAsmCode.getSourceCode().get(0);
-	RowData convertedRow1 = convertedAsmCode.getSourceCode().get(1);
-	Assert.assertEquals("LDB", convertedRow0.getMneumonic());
-	Assert.assertEquals("0x40", convertedRow0.getOperand());
-	Assert.assertEquals("LOOP:", convertedRow1.getLabel());
-	Assert.assertEquals("ADDIA", convertedRow1.getMneumonic());
-	Assert.assertEquals("10", convertedRow1.getOperand());
-    }
+	@Test
+	public void readTest() throws IOException, InvalidKeyException,
+			IllegalArgumentException {
+		ASMReader reader = new ASMReader(new File("SampleData/AsmTiny.s"));
+		SourceData convertedAsmCode = reader.read();
+		RowData convertedRow0 = convertedAsmCode.getSourceCode().get(0);
+		RowData convertedRow1 = convertedAsmCode.getSourceCode().get(1);
+		Assert.assertEquals("LDB", convertedRow0.getMneumonic());
+		Assert.assertEquals("0x40", convertedRow0.getOperand());
+		Assert.assertEquals("LOOP:", convertedRow1.getLabel());
+		Assert.assertEquals("ADDIA", convertedRow1.getMneumonic());
+		Assert.assertEquals("10", convertedRow1.getOperand());
+	}
 
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void codeSpaceTest() throws IOException, InvalidKeyException,
-	    IllegalArgumentException {
-	ASMReader reader = new ASMReader(new File("SampleData/AsmSize.s"));
-	@SuppressWarnings("unused")
-	SourceData tooBig = reader.read();
-	Assert.fail();
-    }
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void codeSpaceTest() throws IOException, InvalidKeyException,
+			IllegalArgumentException {
+		ASMReader reader = new ASMReader(new File("SampleData/AsmSize.s"));
+		@SuppressWarnings("unused")
+		SourceData tooBig = reader.read();
+		Assert.fail();
+	}
 
 }
