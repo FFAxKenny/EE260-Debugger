22bc2b8029d751e8400e5279c919b2afe0d4c2fc
Bronson Oka <abunai59@gmail.com>
2012-09-19 08:54:37 -1000

    fix whitespace in RowData

--- src/main/java/fileParser/RowData.java
+++ src/main/java/fileParser/RowData.java
@@ -13,104 +13,102 @@ import Reference.Reference;
  */
 public class RowData {
 
-    private String mneumonic;
-    private String operand;
-    private int size;
+	private String mneumonic;
+	private String operand;
+	private int size;
 
-    private final int min = Integer.parseInt("00", 16);
+	private final int min = Integer.parseInt("00", 16);
 
-    private final int max = Integer.parseInt("FF", 16);
+	private final int max = Integer.parseInt("FF", 16);
 
-    /**
-     * For a row with 2 arguments, it's either (Label Mneumonic) or (Mneumonic
-     * Value). A boolean specifies which type.
-     * 
-     * @param nmncOrLabel
-     * @param nmncOrValue
-     * @param labelDefined
-     */
-    public RowData(String nmnc, String oper) throws InvalidKeyException {
-	mneumonic = checkMneumonic(nmnc);
-	operand = checkOperand(oper);
-	size = 2;
-    }
-
-    /**
-     * For a row with only 1 argument, only one possibility is available.
-     * 
-     * @param nmnc
-     */
-    public RowData(String nmnc) throws InvalidKeyException {
-	operand = null;
-	mneumonic = checkMneumonic(nmnc);
-	size = 1;
-    }
-
-    /**
-     * Empty constructor
-     */
-    public RowData() {
-	operand = null;
-	mneumonic = null;
-    }
-
-    public String getMneumonic() {
-	return mneumonic;
-    }
-
-    public String getOperand() {
-	return operand;
-    }
-
-    public int getSize() {
-	return size;
-    }
-
-    /**
-     * Checks if the label is okay to use.
-     * 
-     * @param label
-     *            the label that needs to be checked
-     * @return the same label if it passes
-     */
-    public static String checkLabel(String label) throws InvalidKeyException {
-	if (label.charAt(label.length() - 1) != ':') {
-	    throw new InvalidKeyException(
-		    "Invalid Label. Probably missing a ':' at the end of the label");
+	/**
+	 * For a row with 2 arguments, it's either (Label Mneumonic) or (Mneumonic
+	 * Value). A boolean specifies which type.
+	 * 
+	 * @param nmncOrLabel
+	 * @param nmncOrValue
+	 * @param labelDefined
+	 */
+	public RowData(String nmnc, String oper) throws InvalidKeyException {
+		mneumonic = checkMneumonic(nmnc);
+		operand = checkOperand(oper);
+		size = 2;
 	}
-	return label;
-    }
 
-    /**
-     * Checks if the mneumonic is okay to use.
-     * 
-     * @param nmnc
-     * @return returns it back if it passes
-     * @throws InvalidKeyException
-     *             if it doesn't pass it throws this
-     */
-    public String checkMneumonic(String nmnc) throws InvalidKeyException {
-	for (int i = 0; i < Reference.MNEUMONIC_MAP.keySet().size(); i++) {
-	    if (!Reference.MNEUMONIC_MAP.containsValue(nmnc)) {
-		throw new InvalidKeyException(
-			"Enter a valid mneumonic, refer to help");
-	    }
+	/**
+	 * For a row with only 1 argument, only one possibility is available.
+	 * 
+	 * @param nmnc
+	 */
+	public RowData(String nmnc) throws InvalidKeyException {
+		operand = null;
+		mneumonic = checkMneumonic(nmnc);
+		size = 1;
 	}
-	return nmnc;
-    }
 
-    public String checkOperand(String value) throws InvalidKeyException {
-	// figure out if it's data(integers) or an address(hex)
-	if (value.contains("0x")) {
-	    // it's an address
-	    String[] modValue = value.split("x");
-	    value = modValue[1];
+	/**
+	 * Empty constructor
+	 */
+	public RowData() {
+		operand = null;
+		mneumonic = null;
 	}
-	int val = Integer.parseInt(value);
-	if (val <= min && val >= max) {
-	    throw new InvalidKeyException(
-		    "Enter a valid address in between 0x00 and 0xFF");
+
+	public String getMneumonic() {
+		return mneumonic;
 	}
-	return Integer.toHexString(val);
-    }
+
+	public String getOperand() {
+		return operand;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	/**
+	 * Checks if the label is okay to use.
+	 * 
+	 * @param label the label that needs to be checked
+	 * @return the same label if it passes
+	 */
+	public static String checkLabel(String label) throws InvalidKeyException {
+		if (label.charAt(label.length() - 1) != ':') {
+			throw new InvalidKeyException(
+					"Invalid Label. Probably missing a ':' at the end of the label");
+		}
+		return label;
+	}
+
+	/**
+	 * Checks if the mneumonic is okay to use.
+	 * 
+	 * @param nmnc
+	 * @return returns it back if it passes
+	 * @throws InvalidKeyException if it doesn't pass it throws this
+	 */
+	public String checkMneumonic(String nmnc) throws InvalidKeyException {
+		for (int i = 0; i < Reference.MNEUMONIC_MAP.keySet().size(); i++) {
+			if (!Reference.MNEUMONIC_MAP.containsValue(nmnc)) {
+				throw new InvalidKeyException(
+						"Enter a valid mneumonic, refer to help");
+			}
+		}
+		return nmnc;
+	}
+
+	public String checkOperand(String value) throws InvalidKeyException {
+		// figure out if it's data(integers) or an address(hex)
+		if (value.contains("0x")) {
+			// it's an address
+			String[] modValue = value.split("x");
+			value = modValue[1];
+		}
+		int val = Integer.parseInt(value);
+		if (val <= min && val >= max) {
+			throw new InvalidKeyException(
+					"Enter a valid address in between 0x00 and 0xFF");
+		}
+		return Integer.toHexString(val);
+	}
 }
